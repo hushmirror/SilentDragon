e7aea9a8a35f8f9fbdbb3248b0fdbc35f6b2367d
Duke Leto <duke@leto.net>
2020-01-08 15:23:45 -0500

    Comment this out for now

--- src/settings.cpp
+++ src/settings.cpp
@@ -401,7 +401,7 @@ bool Settings::isValidAddress(QString addr) {
     QRegExp zsexp("^zs1[a-z0-9]{75}$",  Qt::CaseInsensitive);
     QRegExp ztsexp("^ztestsapling[a-z0-9]{76}", Qt::CaseInsensitive);
     QRegExp texp("^R[a-z0-9]{33}$", Qt::CaseInsensitive);
-    qDebug() << "isValidAddress(" << addr << ")";
+    //qDebug() << "isValidAddress(" << addr << ")";
 
     return  texp.exactMatch(addr) || ztsexp.exactMatch(addr) || zsexp.exactMatch(addr);
 }
