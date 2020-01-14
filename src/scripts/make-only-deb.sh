8744cdd7e9ed101912961145f4fca298f46b8865
Duke Leto <duke@leto.net>
2020-01-14 10:32:10 -0500

    Update script to make Debian packages

--- src/scripts/make-only-deb.sh
+++ src/scripts/make-only-deb.sh
@@ -2,52 +2,39 @@
 # Copyright (c) 2020 The Hush developers
 # Released under the GPLv3
 
-DEBLOG=deb.log.$$
+echo "Let There Be Debian Packages"
+set -e
+#set -x
+
+ARCH=$(uname -i)
 APP_VERSION=$(cat src/version.h | cut -d\" -f2)
 if [ -z $APP_VERSION ]; then echo "APP_VERSION is not set"; exit 1; fi
 
 # This assumes we already have a staticly compiled SD
 
-echo -n "Packaging.............."
-APP=SilentDragon-v$APP_VERSION
-DIR=$APP
-mkdir $DIR
-strip silentdragon
-
-cp silentdragon   $DIR
-cp komodod        $DIR
-cp komodo-cli     $DIR
-cp komodo-tx      $DIR
-cp hushd          $DIR
-cp hush-cli       $DIR
-cp hush-tx        $DIR
-cp README.md      $DIR
-cp LICENSE        $DIR
-#TODO: and param files
-
-tar czf $APP.tar.gz $DIR/
-cd ..
-
-echo -n "Building deb..........."
-debdir=bin/deb/silentdragon-v$APP_VERSION
+echo "Building Debian package for $APP_VERSION-$ARCH..."
+debdir=deb-SilentDragon-v$APP_VERSION-$ARCH
+if [ -e $debdir ]; then
+    mv $debdir $debdir-backup.$(perl -e 'print time')
+fi
 mkdir -p $debdir > /dev/null
 mkdir    $debdir/DEBIAN
 mkdir -p $debdir/usr/local/bin
 
 cat src/scripts/control | sed "s/RELEASE_VERSION/$APP_VERSION/g" > $debdir/DEBIAN/control
 
+# might have been done already, but just in case
+strip silentdragon
 cp silentdragon                   $debdir/usr/local/bin/
-# TODO: how does this interact with hushd deb ?
-cp $HUSH_DIR/artifacts/komodod $debdir/usr/local/bin/hush-komodod
 
-mkdir -p $debdir/usr/share/pixmaps/
-cp res/silentdragon.xpm           $debdir/usr/share/pixmaps/
+# TODO: We need an updated pixmap!
+#mkdir -p $debdir/usr/share/pixmaps/
+#cp res/silentdragon.xpm           $debdir/usr/share/pixmaps/
 
 mkdir -p $debdir/usr/share/applications
 cp src/scripts/desktopentry    $debdir/usr/share/applications/silentdragon.desktop
 
 dpkg-deb --build $debdir >/dev/null
-cp $debdir.deb                 artifacts/$DIR.deb
-echo "[OK]"
+sha256sum  $debdir.deb
 
 exit 0
