84479022654b194a98b7eb1b9d9503a77e8ffa6c
Charles <18726788+csharpee@users.noreply.github.com>
2020-05-01 14:54:46 -0400

    fixed theme animation startup bug on switch from dark to light

--- src/connection.cpp
+++ src/connection.cpp
@@ -23,7 +23,8 @@ ConnectionLoader::ConnectionLoader(MainWindow* main, RPC* rpc) {
     QMovie *movie2 = new QMovie(":/img/res/silentdragon-animated-startup-dark.gif");;
     auto theme = Settings::getInstance()->get_theme_name();
     auto size  = QSize(512,512);
-    if (theme == "dark" || "midnight") {
+
+    if (theme == "dark" || theme == "midnight") {
         movie2->setScaledSize(size);
         connD->topIcon->setMovie(movie2);
         movie2->start();
